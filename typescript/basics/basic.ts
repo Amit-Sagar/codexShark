72f761d8c0c74fa58e7dc981cb76966d6f040b04
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-03-10 18:53:38 +0530

    tupple, class, private,public,type-aliases

--- typescript/basics/basic.ts
+++ typescript/basics/basic.ts
@@ -1,16 +1,16 @@
 //* string
 let userName1 = "Dave";
-console.log(userName)
+console.log(userName1)
 
 let str1:string;
-str='Faizan';
-console.log(str)
+str1='Faizan';
+console.log(str1)
 
 
 //* number
 let num1:number;
-num=12;
-console.log(num)
+num1=12;
+console.log(num1)
 
 
 //* boolean
@@ -19,8 +19,8 @@ let bool1:boolean = true;
 
 //* array
 let arr1:string[];
-arr=["Faizan", "Siddiqui"];
-console.log(arr)
+arr1=["Faizan", "Siddiqui"];
+console.log(arr1)
 
 let numarr:number[];
 numarr = [23,34,12,56,23,4]
@@ -36,7 +36,7 @@ obj1={
 name:'Faizan',
 age:23
 }
-console.log(obj)
+console.log(obj1)
 
 
 
@@ -94,9 +94,177 @@ function createUser(user:User):User{
 
 createUser({name:"faizan", id:23})
 
+type MyAdmin ={
+  name:string;
+  email:string;
+  isActive:boolean
+}
+
+const iAdmin: MyAdmin={
+  name:"Faizan",
+  email:"abc@gmail.com",
+  isActive:true,
+}
+
+type cardNumber = {
+  cardNum: string
+}
+type cardDate ={
+  carddate:string
+}
+
+type cardDetails = cardNumber & cardDate & {
+  cvv:number
+}
+
+let cardUser : cardDetails = {
+  cardNum:"2354656",
+  carddate:"Aug 2023",
+  cvv:444
+}
+
+
+type Animal = {
+  color:string;
+  name:string;  
+}
+
+
+type Bird = {
+  species:string;
+  num:number;
+}
+
+let mtPet : Animal & Bird ={
+  color:'black',
+  name:'Daizy',
+  species:'Dog',
+  num:1,
+}
+
 // *Union => we can define two or more data types
 
 let id3:string | number
 id3 = 23
 id3="234"
+
+// array of different types
+let unionArr:(string | number)[];
+unionArr = ["Faizan", "Siddiqui", 123]
+
+let unionArry:(string | number | boolean)[];
+unionArry = [false,'Hey',13];
+
+
+// *Tuple
+// restricted to order od types
+
+let tUser:[string, number];
+tUser = ["Faizan", 12]       //(✔)
+// tUser = [12,"Faizan"]     (×) (order is different of types so its not possible)
+
+
+let tUser1:[string, number, boolean];
+tUser1 = ["Faizan", 12, true]
+
+type newUser = [number, string]
+let user1:newUser = [112,'abc@google.com']
+user1[1] = "example@google.com"    //We can mutate the tuple
+
+// we can apply array methods here like {push, pop, shift, unshift }
+user1.push(12)
+user1.push('hello')
+
+
+// *Interface
+interface mUser {
+  dbId:number,
+  email:string,
+  userId:number,
+
+  // method 1
+  startTrail:() => string
+
+  // method 2
+  getName():number
+
+  getCoupon(coupanName:string,value:number):string,
+}
+
+
+// 1) Opening of a interface
+// we can add property in existing interface by writing it again
+interface mUser{
+  githubId:string
+}
+
+// 2) Inheritence
+interface Admin extends mUser {
+  role:"admin" | "ta" | "learner"
+}
+
+const faizan: mUser = {
+  dbId:22,
+  email:'abc@gmail.com',
+  userId:2132,
+  githubId:'scloijcegiw',
+  startTrail:()=> {
+    return "Trail started"
+  },
+  getName:()=> {
+    return 12
+  },
+  getCoupon:(name:"Faizan",off:10)=> {
+    return '10'
+  }
+}
+
+const hitesh: Admin = {
+  dbId:22,
+  email:'abc@gmail.com',
+  userId:2132,
+  githubId:'scloijcegiw',
+  role:'admin',
+  startTrail:()=> {
+    return "Trail started"
+  },
+  getName:()=> {
+    return 12
+  },
+  getCoupon:(name:"Faizan",off:10)=> {
+    return '10'
+  }
+}
+
+// *class
+class team {
+  name:string
+  email:string
+  constructor(email:string,name:string){
+    this.email = email;
+    this.name= name
+  }
+}
+const x = new team('f@faizan.com','Fazain')
+
+// public, private, protected 
+// by default every property and method are public
+class team1 {
+  name:string
+  email:string
+  private id:number
+  constructor(email:string,name:string){
+    this.email = email;
+    this.name= name
+  }
+}
+const shark = new team('f@google.com','Faizan')
+
+
+
+
+
+
+
+
 
